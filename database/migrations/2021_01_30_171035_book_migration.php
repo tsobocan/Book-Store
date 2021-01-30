5d423e0900fea56accf92e87733c98239a60850f
tsobocan <tsobocan@users.noreply.github.com>
2021-01-30 18:26:48 +0100

    Update 2021_01_30_171035_book_migration.php

--- database/migrations/2021_01_30_171035_book_migration.php
+++ database/migrations/2021_01_30_171035_book_migration.php
@@ -4,16 +4,43 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class BookMIgration extends Migration
+class BookMigration extends Migration
 {
     /**
-     * Run the migrations.
-     *
-     * @return void
+     * Note : all migrations are intentionally used in one migrations.
      */
+
     public function up()
     {
-        //
+
+     /*   Schema::create('authors', function(Blueprint $table){
+            $table->id();
+            $table->string('name');
+            $table->string('surname');
+            $table->timestamps();
+        });*/
+
+        Schema::create('books', function(Blueprint $table){
+            $table->id();
+            $table->string('title');
+            $table->foreignId('author_id')->constrained('authors');
+            $table->integer('year');
+            $table->timestamps();
+        });
+
+        Schema::create('statuses', function(Blueprint $table){
+            $table->id();
+            $table->string('title');
+        });
+
+        Schema::create('actions', function(Blueprint $table){
+            $table->id();
+            $table->foreignId('book_id')->constrained('books');
+            $table->foreignId('status_id')->constrained('statuses');
+            $table->dateTime('valid_from');
+            $table->dateTime('valid_to');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +50,10 @@ class BookMIgration extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('actions');
+        Schema::dropIfExists('books');
+        Schema::dropIfExists('authors');
+        Schema::dropIfExists('statuses');
+
     }
 }
