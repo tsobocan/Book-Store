ece7cad3f1b88c78316632d15995744429f145c4
tsobocan <tsobocan@users.noreply.github.com>
2021-02-01 02:34:33 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,23 +20,26 @@
         return Inertia::render('Home', [
             'canLogin' => Route::has('login'),
             'canRegister' => Route::has('register'),
-            'laravelVersion' => Application::VERSION,
-            'phpVersion' => PHP_VERSION,
         ]);
     })->name('home');
-
-    Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
-
-    Route::middleware('auth:sanctum')->post('/book/action', [HomeController::class, 'createAction'])->name('action');
-    Route::middleware('auth:sanctum')->post('/book/save', [HomeController::class, 'editBook'])->name('edit');
-    Route::middleware('auth:sanctum')->post('/book/delete', [HomeController::class, 'deleteBook'])->name('delete');
-    Route::middleware('auth:sanctum')->post('/users', [HomeController::class, 'fetchUsers'])->name('users');
-    Route::middleware('auth:sanctum')->get('/books/active', [HomeController::class, 'activeBooks'])->name('active');
-    Route::middleware('auth:sanctum')->post('/books/rent', [HomeController::class, 'rent'])->name('rent');
-    Route::middleware('auth:sanctum')->post('/books/return', [HomeController::class, 'returnBook'])->name('return');
 
     Route::get('/actions', function () {
         return Inertia::render('Actions');
     })->name('actions');
+
+    /*
+     * This could also be done using ResourceControllers, but I don't like them.
+     */
+    Route::middleware('auth:sanctum')->group(function(){
+        Route::post('/book/action', [HomeController::class, 'createAction'])->name('action');
+        Route::post('/book/save', [HomeController::class, 'editBook'])->name('edit');
+        Route::post('/book/delete', [HomeController::class, 'deleteBook'])->name('delete');
+        Route::post('/users', [HomeController::class, 'fetchUsers'])->name('users');
+        Route::get('/books/active', [HomeController::class, 'activeBooks'])->name('active');
+        Route::post('/books/rent', [HomeController::class, 'rent'])->name('rent');
+        Route::post('/books/return', [HomeController::class, 'returnBook'])->name('return');
+    });
+
+
+
+
